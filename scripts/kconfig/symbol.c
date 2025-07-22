4ec349c48dd861915c206de5f5989e12443acacf
computerqwq <87743828+jisuanjiQAQ@users.noreply.github.com>
2025-07-22 13:16:37 +0800

    Update symbol.c

--- scripts/kconfig/symbol.c
+++ scripts/kconfig/symbol.c
@@ -1226,6 +1226,7 @@ static struct symbol *sym_check_choice_deps(struct symbol *choice)
 	struct property *prop;
 	struct expr *e;
 	struct dep_stack stack;
+	static struct dep_stack stack;
 
 	dep_stack_insert(&stack, choice);
 
